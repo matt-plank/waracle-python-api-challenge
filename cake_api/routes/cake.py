ef36a6c17822e649ff122a92df98de5f5dd740bc
matt-plank <matthew_plank@outlook.com>
2023-10-24 14:16:13 +0100

    Added pydantic model for cake

--- cake_api/routes/cake.py
+++ cake_api/routes/cake.py
@@ -1,23 +1,28 @@
 from fastapi import APIRouter
-from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 
 router = APIRouter()
 
 
-CAKES: list[dict] = [
-    {
-        "id": 1,
-        "name": "Chocolate Cake",
-        "comment": "A simple chocolate cake",
-        "imageURL": "https://food-images.files.bbci.co.uk/food/recipes/easy_chocolate_cake_31070_16x9.jpg",
-        "yumFactor": 5,
-    }
+class Cake(BaseModel):
+    id: int
+    name: str
+    comment: str
+    imageUrl: str
+    yumFactor: int
+
+
+CAKES: list[Cake] = [
+    Cake(
+        id=1,
+        name="Chocolate Cake",
+        comment="A simple chocolate cake",
+        imageUrl="https://food-images.files.bbci.co.uk/food/recipes/easy_chocolate_cake_31070_16x9.jpg",
+        yumFactor=5,
+    )
 ]
 
 
 @router.get("")
 async def get_many():
-    return JSONResponse(
-        status_code=200,
-        content=CAKES,
-    )
+    return CAKES
